814765210c7efdea3f170180c4ec9138e5b2d74d
seanyoung247 <seanyoung247@gmail.com>
2021-03-28 22:53:30 +0100

    Fixes build error

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-if os.path.exists('env.py')
+if os.path.exists('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
